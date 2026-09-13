f9cbcb9e07ce8269a14875055d4561facf90738f
Waseem <waseem@ludokingadda.com>
2026-09-13 07:19:21 +0530

    chore: Sync latest asset updates and push clean build to GitHub

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m77ki1fu3ic"
+    "revision": "0.0gk5urdoj2o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
